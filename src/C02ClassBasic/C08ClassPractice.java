89d341cc6a163aaaeaa5cdadb03eb8ce068c1147
fojf <john9823@naver.com>
2025-06-19 17:09:29 +0900

    수업 내용 정리
    8. 클래스, 객체 연습
    9. 은행 서비스를 구현해보면서 클래스(BankAccount)를 구현, 활용하는 연습
    10. Author, Post 클래스를 구현하고 활용하는 연습

--- src/C02ClassBasic/C08ClassPractice.java
+++ src/C02ClassBasic/C08ClassPractice.java
@@ -1,19 +1,65 @@
 package C02ClassBasic;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class C08ClassPractice {
     public static void main(String[] args) {
-        List<Account> myList = new ArrayList<>();
-        Account account1 = new Account("hong1", "1234-4231-1234", 120003420);
-        Account account2 = new Account("hong2", "3123-4231-1234", 1303420);
+//        List<Account> myList = new ArrayList<>();
+//        Account a1 = new Account("hong1", "1234-4231-1234", 100000);
+//        Account a2 = new Account("hong2", "3123-4231-1234", 200000);
+//
+//        myList.add(a1);
+//        myList.add(a2);
+//
+////        계좌 번호 목록 출력
+//        for (Account a : myList) {
+//            System.out.println(a.getAccountNumber());
+//        }
+//
+////        사용자1이 사용자2에게 송금
+//        boolean isSuccess = true;
+//        int amount = 500000;
+//        for (Account a : myList) {
+//            if (a.getAccountNumber().equals("1234-4231-1234")) {
+//                isSuccess = a.updateBalance(-amount);
+//                System.out.println("잔액 부족");
+//                break;
+//            }
+//        }
+//
+//        for (Account a : myList) {
+//            if (isSuccess && a.getAccountNumber().equals("3123-4231-1234")) {
+//                a.updateBalance(amount);
+//                break;
+//            }
+//        }
 
-        myList.add(account1);
-        myList.add(account2);
+////        계좌 상세 조회 : 계좌 번호와 잔고 조회
+//        for (Account a : myList) {
+//            System.out.println(a);
+//        }
 
-        System.out.println(myList.get(0));
-        System.out.println(myList.get(1));
+//        Map으로 저장
+        Map<String, Account> accountMap = new HashMap<>();
+
+        accountMap.put("12345", new Account("hong1", "12345", 300000));
+        accountMap.put("54321", new Account("hong2", "54321", 100000));
+
+//      송금 로직
+        Account from = accountMap.get("12345");
+        Account to = accountMap.get("54321");
+
+        int amount = 500000;
+        if (from.updateBalance(-amount)) to.updateBalance(amount);
+        else System.out.println("잔고 부족!");
+
+//        계좌 상세 조회
+        for (Account acc : accountMap.values()) {
+            System.out.println(acc);
+        }
     }
 }
 
@@ -44,8 +90,16 @@ class Account {
         return balance;
     }
 
+    public boolean updateBalance(long amount) {
+        if (amount < 0 && this.balance < -amount) return false;
+        this.balance += amount;
+        return true;
+    }
+
     @Override
     public String toString() {
-        return "예금주 : " + name + ", 계좌 번호 : " + accountNumber + ", 잔고 : " + balance;
+        return "예금주 : " + name + ", 계좌 번호 : " + accountNumber + ", 잔고 : " + balance + "원";
     }
+
+
 }
